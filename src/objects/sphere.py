d7782cfeb7471ff9f7b538f227b9b39bb15e0acb
Riku Arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2018-12-28 21:47:47 +0900

    Implement collision check and reward setting

--- src/objects/sphere.py
+++ src/objects/sphere.py
@@ -9,3 +9,8 @@ class SolidSphere(ObjectBase):
     def __init__(self, radius, mass=1.0, color=np.zeros(3)):
         super(SolidSphere, self).__init__(mass, color)
         self.radius = radius
+
+    def update_dynamics(self, dt, new_velocity=None, angular_velocity=None):
+        self.position += self.velocity * dt
+        if new_velocity is not None:
+            self.velocity = new_velocity
