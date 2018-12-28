e00b284a68e2755294e98f767b0908980c4e371f
Masaya Kaneko <syinari0123@gmail.com>
2018-12-28 23:57:31 +0900

    enable network inference

--- src/agents/pipeline.py
+++ src/agents/pipeline.py
@@ -1,4 +1,10 @@
 import time
+import cv2
+import torch
+
+from bindsnet.network.nodes import Input, AbstractInput
+
+from env import events_to_image
 
 class Pipeline(object):
     def __init__(self, network, environment, encoding, action_function, output):
@@ -13,6 +19,10 @@ class Pipeline(object):
         self.save_interval = 1
         self.save_dir = 'network.pt'
 
+        self.time = 1
+        self.dt = network.dt
+        self.timestep = int(self.time / self.dt)
+
         # variables to use in this pipeline
         self.episode = 0
         self.iteration = 0
@@ -22,6 +32,11 @@ class Pipeline(object):
         self.obs = None
         self.reward = None
         self.done = None
+
+        # Set up for multiple layers of input layers.
+        self.encoded = {
+            name: torch.Tensor() for name, layer in network.layers.items() if isinstance(layer, AbstractInput)
+        }
 
         self.clock = time.time()
 
@@ -41,14 +56,17 @@ class Pipeline(object):
         #a = self.action_function(self, output=self.output)
         
         # Run a step of the environment.
-        self.obs, self.reward, self.done, info = self.env.step(action='forward')
+        events, self.reward, self.done, info = self.env.step(action='forward')
+        # currently image-based learning is adopted (Future work : spike-based)
+        events_img = events_to_image(events, self.env.render_width, self.env.render_height)
+        self.obs = torch.from_numpy(cv2.cvtColor(events_img, cv2.COLOR_BGR2GRAY)).float()/255.0
 
         # Encode the observation using given encoding function.
-        #for inpt in self.encoded:
-        #    self.encoded[inpt] = self.encoding(self.obs, time=self.time, dt=self.network.dt, **kwargs)
+        for inpt in self.encoded:
+            self.encoded[inpt] = self.encoding(self.obs, time=self.time, dt=self.network.dt)
 
         # Run the network on the spike train-encoded inputs.
-        #self.network.run(inpts=self.encoded, time=self.time, reward=self.reward, **kwargs)
+        self.network.run(inpts=self.encoded, time=self.time, reward=self.reward)
 
         self.iteration += 1
 
@@ -57,21 +75,10 @@ class Pipeline(object):
             self.episode += 1
             self.accumulated_reward = 0
 
-
-    def reset_(self) -> None:
-        # language=rst
+    def reset_(self):
         """
         Reset the pipeline.
         """
         self.env.reset()
         self.network.reset_()
         self.iteration = 0
-
-
-    def reference(self):
-        events, r, done, info = env.step(action='forward')
-        image = events_to_image(events)
-        print(image.shape)
-
-        cv2.imwrite("../fig/image" + str(i) + ".png", image)
-        print("Average Elapsed Time: {} s".format((time.time() - start) / N))
--- src/train.py
+++ src/train.py
@@ -8,26 +8,23 @@ from agents import create_network, Pipeline, select_softmax
 from env import FallingStone, events_to_image
 
 
-"""
-Now I'm debugging in following environment...
-renderer.display_width = 80
-renderer.display_height = 80
-"""
-
 def main():
+    # display size
+    w, h = 80, 80
+
     # Build network
     network = create_network()
 
     # Load environment
-    environment = FallingStone()
+    environment = FallingStone(render_width=w, render_height=h)
 
     # Build pipeline from specified components.
     pipeline = Pipeline(network, environment, encoding=bernoulli,
                         action_function=select_softmax, output='Output Layer')
 
     # Run environment simulation for 100 episodes.
-    pipeline.reset_()
-    for i in range(100):
+    #pipeline.reset_()
+    for i in range(10):
         # initialize episode reward
         reward = 0
         # pipeline.reset_() # including bug
