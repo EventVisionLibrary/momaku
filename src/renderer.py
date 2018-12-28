1378c7e99d9fc673298f239d819fc000b2eac04e
Riku Arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2018-12-28 16:11:50 +0900

    Add rendering sphere

--- src/renderer.py
+++ src/renderer.py
@@ -1,9 +1,11 @@
 import cv2
+from math import pi, cos, sin
 import numpy as np
 import time
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
 import glfw
 
 from objects.cube import SolidCube
@@ -71,9 +73,44 @@ class Renderer():
         self.setup_perspective()
 
     def __draw_sphere(self, sphere):
-        raise NotImplementedError
+        # divide into triangles
+        def f(u, v):
+            r = sphere.radius
+            x, y, z = sphere.position
+            return np.array([x+r*sin(v)*cos(u), y+r*cos(v), z+r*sin(v)*sin(u)]) # quadratic
+
+        start_u = 0
+        start_v = 0
+        end_u = 2*pi
+        end_v = pi
+        resolution_u = 20
+        resolution_v = 20
+        for u in np.linspace(start_u, end_u, resolution_u):
+            for v in np.linspace(start_v, end_v, resolution_v):
+                u_next = min(end_u, u+(end_u-start_u)/resolution_u)
+                v_next = min(end_v, v+(end_v-start_v)/resolution_v)
+                p0 = f(u, v)
+                p1 = f(u, v_next)
+                p2 = f(u_next, v)
+                p3 = f(u_next, v_next)
+                # 1
+                glBegin(GL_TRIANGLES)
+                glColor3f(*sphere.color)
+                glVertex3f(*p0)
+                glVertex3f(*p2)
+                glVertex3f(*p1)
+                glEnd()
+                # 2
+                glBegin(GL_TRIANGLES)
+                glColor3f(*sphere.color)
+                glVertex3f(*p3)
+                glVertex3f(*p1)
+                glVertex3f(*p2)
+                glEnd()
 
     def __draw_cube(self, cube):
+        glBegin(GL_QUADS)
+        glColor3f(*cube.color)
         vertices = cube.vertices
         # 1
         glVertex3f(*vertices[0])
@@ -105,6 +142,7 @@ class Renderer():
         glVertex3f(*vertices[3])
         glVertex3f(*vertices[7])
         glVertex3f(*vertices[4])
+        glEnd()
 
     def render_objects(self, objects):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -114,33 +152,37 @@ class Renderer():
         glMaterialfv(GL_FRONT, GL_SPECULAR, specular)
         glMaterialfv(GL_FRONT, GL_SHININESS, shininess)
 
-        glBegin(GL_QUADS)
+
         for obj in objects:
-            glColor3f(*obj.color)
             if isinstance(obj, SolidCube):
                 self.__draw_cube(obj)
             elif isinstance(obj, SolidSphere):
                 self.__draw_sphere(obj)
-        glEnd()
+
         image_buffer = glReadPixels(0, 0, self.display_width, self.display_height, OpenGL.GL.GL_RGB, OpenGL.GL.GL_UNSIGNED_BYTE)
         image = np.frombuffer(image_buffer, dtype=np.uint8).reshape(self.display_width, self.display_height, 3)
         return image
 
 if __name__ == "__main__":
-    cube = SolidCube(size=1.0)
-
     camera_position = [-1, -1, -1]
     target_position = [3, 3, 3]
     renderer = Renderer(camera_position=camera_position, target_position=target_position)
+
+    # objects
+    cube = SolidCube(size=1.0)
     cube.vertices = np.array([[2, 2, 0], [2, 2, 2], [2, 6, 2], [2, 6, 0],
                               [4, 2, 0], [4, 2, 2], [4, 6, 2], [4, 6, 0]], dtype=np.float64)
+    sphere = SolidSphere(radius=0.5)
+    sphere.position = np.array([0, 0, 0])
+
     start = time.time()
     N = 20
     for i in range(0, N):
         cube.vertices[:, 1] += 0.2
+        sphere.position[2] += 0.1
         camera_position[0] -= 0.1
         camera_position[1] -= 0.1
         renderer.update_perspective(camera_position, target_position)
-        image = renderer.render_objects([cube])
+        image = renderer.render_objects([cube, sphere])
         cv2.imwrite("../fig/image" + str(i) + ".png", image)
     print("Average Elapsed Time: {} s".format((time.time()-start)/N))
